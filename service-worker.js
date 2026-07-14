c5f0d8427f9e2119d8feb1860c00ea7d8b496e32
AdixGon <9889597880uma@gmail.com>
2026-07-14 09:33:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,65 @@
-const CACHE_NAME = "countdown-v1";
+const CACHE = "countdown-v2";
 
 const FILES = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE)
+
+.then(cache => cache.addAll(FILES))
+
+);
+
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+
+caches.keys().then(keys =>
+
+Promise.all(
+
+keys.map(key => {
+
+if(key!==CACHE){
+
+return caches.delete(key);
+
+}
+
+})
+
+)
+
+)
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
